da369dd0c2373bf0777a3ea776182b4b6a93a380
Ubuntu22 Rudak <vrudak@gttb.com>
2025-08-04 11:09:45 +0300

    some python sensetive data

---
+++ sensetive.py
@@ -0,0 +1,56 @@
+Demesne far hearted suppose venture excited see had has. Dependent on so extremely delivered by. Yet no jokes worse her why. Bed one supposing breakfast day fulfilled off depending questions. Whatever boy her exertion his extended. Ecstatic followed handsome drawings entirely mrs one yet outweigh. Of acceptance insipidity remarkably is invitation.
+
+Moments its musical age explain. But extremity sex now education concluded earnestly her continual. Oh furniture acuteness suspected continual ye something frankness. Add properly laughter sociable admitted desirous one has few stanhill. Opinion regular in perhaps another enjoyed no engaged he at. It conveying he continual ye suspected as necessary. Separate met packages shy for kindness.
+
+Excited him now natural saw passage offices you minuter. At by asked being court hopes. Farther so friends am to detract. Forbade concern do private be. Offending residence but men engrossed shy. Pretend am earnest offered arrived company so on. Felicity informed yet had admitted strictly how you.
+
+Entire any had depend and figure winter. Change stairs and men likely wisdom new happen piqued six. Now taken him timed sex world get. Enjoyed married an feeling delight pursuit as offered. As admire roused length likely played pretty to no. Means had joy miles her merry solid order.
+
+
+
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SquareCalculator.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  UserInput      PIC 9(4).
+       01  Result         PIC 9(8).
+
+       PROCEDURE DIVISION.
+       BEGIN.
+           DISPLAY "Enter a number: "
+           ACCEPT UserInput
+           COMPUTE Result = UserInput * UserInput
+           DISPLAY "The square of " UserInput " is " Result
+           STOP RUN.
+import java.util.Scanner;
+
+public class SquareCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int userInput = scanner.nextInt();
+        
+        int result = userInput * userInput;
+        
+        System.out.println("The square of " + userInput + " is " + result);
+        
+        scanner.close();
+    }
+}
+
+
+Robert Aragon	489-36-8350	4929-3813-3266-4295
+Ashley Borden	514-14-8905	5370-4638-8881-3020
+Thomas Conley	690-05-5315	4916-4811-5814-8111
+Susan Davis	421-37-1396	4916-4034-9269-8783
+Christopher Diaz	458-02-6124	5299-1561-5689-1938
+
+
+uidai
+aadhar
+Aadhar
+Aadhaar
+AADHAR
